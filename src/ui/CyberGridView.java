31b8a30dd80fbd9c309e72913d42e16365d95946
Nerhart11 <n_erhart@icloud.com>
2026-05-28 17:17:23 -0700

    Merger

--- src/ui/CyberGridView.java
+++ src/ui/CyberGridView.java
@@ -1,4 +1,18 @@
 package ui;
 
-public class CyberGridView {
+import java.awt.*;
+import javax.swing.*;
+
+public class CyberGridView 
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Network Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(900, 600));
+        
+        frame.setVisible(true);
+        
+        
+    }
 }
